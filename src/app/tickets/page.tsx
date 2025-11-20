e594b8c04ce36f7050161a53ada2d75446b3f1e6
jorgekairiyama <jorge.kairiyama@gmail.com>
2025-11-20 10:01:15 -0300

    41 Tailwind CSS in Next

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,25 +2,30 @@ import Link from "next/link";
 import { initialTickets } from "@/src/data";
 import { ticketPath } from "@/src/paths";
 
-const TICKET_ICONS = {
-    OPEN: "O",
-    DONE: "X",
-    IN_PROGRESS: ">",
-};
 
 const TicketsPage = () => {
     return (
-        <div>
-            {initialTickets.map((ticket) => (
-                <div key={ticket.id}>
-                    <div>{TICKET_ICONS[ticket.status]}</div>
-                    <h2 className="text-lg">{ticket.title}</h2>
-
-                    <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                        View
-                    </Link>
+        < div className="flex-1 flex flex-col gap-y-8">
+            < div className="flex-1 flex flex-col gap-y-8">
+                <div>
+                    <h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
+                    <p className="text-sm text-muted-foreground">
+                        All your tickets at one place
+                    </p>
                 </div>
-            ))}
+            </div>
+            <div className="flex-1 flex flex-col items-center gap-y-4">
+                {initialTickets.map((ticket) => (
+                    <div key={ticket.id} className="w-full max-w-[420px] p-4 border border-red-100 rounded">
+                        <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
+                        <p className="text-sm text-slate-500 truncate">{ticket.content}</p>
+
+                        <Link href={ticketPath(ticket.id)} className="text-sm underline">
+                            View
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
